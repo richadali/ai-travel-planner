b68ed10a2a5f831a8f7674a8257f797ed99dd5d7
richadali <richadyaminali@gmail.com>
2025-07-20 03:07:38 +0530

    Update ESLint configuration to disable specific TypeScript rules for node_modules, .next, dist, and build directories, enhancing linting flexibility and reducing false positives.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+    files: ["**/node_modules/**", "**/.next/**", "**/dist/**", "**/build/**"],
+  },
 ];
 
 export default eslintConfig;
